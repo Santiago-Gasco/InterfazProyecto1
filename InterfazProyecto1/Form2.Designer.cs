68f1f7ef326433068e83643d74695cdf4c84b36c
56249478 <56249478@T140J2312078204>
2024-08-12 14:56:41 -0300

    g

--- InterfazProyecto1/Form2.Designer.cs
+++ InterfazProyecto1/Form2.Designer.cs
@@ -228,7 +228,7 @@ namespace InterfazProyecto1
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(346, 136);
+            this.button5.Location = new System.Drawing.Point(511, 149);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 10;
